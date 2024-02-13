519226c24b0deadfe7b6aaaeb3744d0acfe06687
OliBomby <olivierschipper@gmail.com>
2024-02-13 20:14:25 +0100

    add class embedding to input

--- osuT5/model/osu_t.py
+++ osuT5/model/osu_t.py
@@ -18,12 +18,18 @@ class OsuT(nn.Module):
         self.spectrogram = MelSpectrogram(
             config.sample_rate, config.n_fft, config.n_mels, config.hop_length
         )
-        self.encoder_embedder = nn.Linear(config.n_mels, config.d_model)
+        self.style_embedder = LabelEmbedder(config.num_classes, config.d_model, config.class_dropout_prob)
+        self.encoder_embedder = nn.Linear(config.n_mels + config.d_model, config.d_model)
+
+        # Initialize label embedding table:
+        nn.init.normal_(self.style_embedder.embedding_table.weight, std=0.02)
+
         self.transformer = T5(config)
 
     def forward(
             self,
             samples: Optional[torch.FloatTensor] = None,
+            styles: Optional[torch.LongTensor] = None,
             encoder_outputs=None,
             **kwargs
     ) -> Seq2SeqLMOutput:
@@ -36,8 +42,10 @@ class OsuT(nn.Module):
         inputs_embeds = None
 
         if encoder_outputs is None:
-            frames = self.spectrogram(samples)
-            inputs_embeds = self.encoder_embedder(frames)
+            frames = self.spectrogram(samples)  # (N, L, M)
+            style_embeds = self.style_embedder(styles, self.training)  # (N, D)
+            frames_concat = torch.concatenate((frames, style_embeds.unsqueeze(1)), -1)
+            inputs_embeds = self.encoder_embedder(frames_concat)
 
         output = self.transformer.forward(inputs_embeds=inputs_embeds, encoder_outputs=encoder_outputs, **kwargs)
 
@@ -71,3 +79,39 @@ def get_state_dict_part(state_dict: Mapping[str, Any], part: str):
             sub_dist[k[i + 1:]] = state_dict[k]
 
     return sub_dist
+
+
+class LabelEmbedder(nn.Module):
+    """
+    Embeds class labels into vector representations. Also handles label dropout.
+    """
+
+    def __init__(self, num_classes, hidden_size, dropout_prob):
+        super().__init__()
+        use_cfg_embedding = dropout_prob > 0
+        self.embedding_table = nn.Embedding(
+            num_classes + use_cfg_embedding,
+            hidden_size,
+        )
+        self.num_classes = num_classes
+        self.dropout_prob = dropout_prob
+
+    def token_drop(self, labels, force_drop_ids=None):
+        """
+        Drops labels.
+        """
+        if force_drop_ids is None:
+            drop_ids = (
+                torch.rand(labels.shape[0], device=labels.device) < self.dropout_prob
+            )
+        else:
+            drop_ids = force_drop_ids == 1
+        labels = torch.where(drop_ids, self.num_classes, labels)
+        return labels
+
+    def forward(self, labels, train, force_drop_ids=None):
+        use_dropout = self.dropout_prob > 0
+        if (train and use_dropout) or (force_drop_ids is not None):
+            labels = self.token_drop(labels, force_drop_ids)
+        embeddings = self.embedding_table(labels)
+        return embeddings
