c2e43b43b1e921fcdddb09cbda037d50a1cc42df
OliBomby <olivierschipper@gmail.com>
2024-03-13 17:22:34 +0100

    fix inference v2 2

--- inference.py
+++ inference.py
@@ -36,15 +36,19 @@ def main(args: DictConfig):
 
     torch.set_grad_enabled(False)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    checkpoint = torch.load(args.model_path, map_location=device)
-    t5_config = get_config(args)
+    ckpt_path = Path(args.model_path)
+    model_state = torch.load(ckpt_path / "pytorch_model.bin", map_location=device)
+    tokenizer_state = torch.load(ckpt_path / "custom_checkpoint_0.pkl")
+
+    tokenizer = Tokenizer()
+    tokenizer.load_state_dict(tokenizer_state)
+    t5_config = get_config(args, tokenizer)
 
     model = get_model(t5_config)
-    model.load_state_dict(checkpoint)
+    model.load_state_dict(model_state)
     model.eval()
     model.to(device)
 
-    tokenizer = Tokenizer()
     preprocessor = Preprocessor(args)
     pipeline = Pipeline(args, tokenizer)
     postprocessor = Postprocessor(args)
