fe9dfe624d8ce08ac66754a0dcc9df3b133bbe6e
OliBomby <olivierschipper@gmail.com>
2024-04-18 13:58:33 +0200

    optimize item() call for loss metric

--- osuT5/utils/log_utils.py
+++ osuT5/utils/log_utils.py
@@ -1,28 +1,44 @@
 from collections import defaultdict
 
 import numpy as np
+import torch
 
 
 class Averager:
     def __init__(self):
         self.reset()
 
+    # noinspection PyAttributeOutsideInit
     def reset(self):
-        self.total = defaultdict(float)
-        self.counter = defaultdict(float)
+        self.total = {}
+        self.counter = {}
 
     def update(self, stats):
         for key, value in stats.items():
-            if isinstance(value, np.ndarray):
-                self.total[key] = self.total[key] + value.sum()
-                self.counter[key] = self.counter[key] + value.size
+            if key not in self.total:
+                if isinstance(value, torch.Tensor):
+                    self.total[key] = value.sum()
+                    self.counter[key] = value.numel()
+                elif isinstance(value, np.ndarray):
+                    self.total[key] = value.sum()
+                    self.counter[key] = value.size
+                else:
+                    self.total[key] = value
+                    self.counter[key] = 1
             else:
-                self.total[key] = self.total[key] + value
-                self.counter[key] = self.counter[key] + 1
+                if isinstance(value, torch.Tensor):
+                    self.total[key] = self.total[key] + value.sum()
+                    self.counter[key] = self.counter[key] + value.numel()
+                elif isinstance(value, np.ndarray):
+                    self.total[key] = self.total[key] + value.sum()
+                    self.counter[key] = self.counter[key] + value.size
+                else:
+                    self.total[key] = self.total[key] + value
+                    self.counter[key] = self.counter[key] + 1
 
     def average(self):
         averaged_stats = {
-            key: tot / self.counter[key] for key, tot in self.total.items()
+            key: (tot / self.counter[key]).item() if isinstance(tot, torch.Tensor) else tot / self.counter[key] for key, tot in self.total.items()
         }
         self.reset()
 
