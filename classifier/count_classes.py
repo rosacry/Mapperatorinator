3f4ad7f01c7c4378bf254ebafe55025377334914
OliBomby <olivierschipper@gmail.com>
2024-10-19 20:35:35 +0200

    add beatmap mapper classifier

---
+++ classifier/count_classes.py
@@ -0,0 +1,49 @@
+import json
+from pathlib import Path
+
+
+def init_mapper_idx(mappers_path):
+    """"Indexes beatmap mappers and mapper idx."""
+    path = Path(mappers_path)
+
+    if not path.exists():
+        raise ValueError(f"mappers_path {path} not found")
+
+    # Load JSON data from file
+    with open(path, 'r') as file:
+        data = json.load(file)
+
+    # Populate beatmap_mapper
+    beatmap_mapper = {}
+    for item in data:
+        beatmap_mapper[item['id']] = item['user_id']
+
+    # Get unique user_ids from beatmap_mapper values
+    unique_user_ids = list(set(beatmap_mapper.values()))
+
+    # Create mapper_idx
+    mapper_idx = {user_id: idx for idx, user_id in enumerate(unique_user_ids)}
+    num_mapper_classes = len(unique_user_ids)
+
+    return beatmap_mapper, mapper_idx, num_mapper_classes
+
+
+path = "../datasets/beatmap_users.json"
+beatmap_mapper, mapper_idx, num_mapper_classes = init_mapper_idx(path)
+
+print("Number of mapper classes:", num_mapper_classes)
+# Calculate number of maps per mapper
+maps_per_mapper = {}
+for beatmap_id in beatmap_mapper:
+    user_id = beatmap_mapper[beatmap_id]
+    if user_id not in maps_per_mapper:
+        maps_per_mapper[user_id] = 0
+    maps_per_mapper[user_id] += 1
+
+# Calculate average maps per mapper class
+average_maps_per_mapper = len(beatmap_mapper) / num_mapper_classes
+print("Average maps per mapper class:", average_maps_per_mapper)
+
+# Calculate median maps per mapper class
+median_maps_per_mapper = sorted(maps_per_mapper.values())[num_mapper_classes // 2]
+print("Median maps per mapper class:", median_maps_per_mapper)
---
+++ classifier/libs/__init__.py
@@ -0,0 +1,1 @@
+from .utils.model_utils import get_dataloaders, get_model, get_optimizer, get_scheduler, get_tokenizer
