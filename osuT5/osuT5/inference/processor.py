c33b4289b1fbe809f3686d64520c71f7c7da0d93
OliBomby <olivierschipper@gmail.com>
2024-12-01 00:38:55 +0100

    remove unused _filter method

--- osuT5/osuT5/inference/processor.py
+++ osuT5/osuT5/inference/processor.py
@@ -512,42 +512,6 @@ class Processor(object):
 
         return events
 
-    def _filter(
-            self, logits: torch.Tensor, top_p: float, filter_value: float = -float("Inf")
-    ) -> torch.Tensor:
-        """Filter a distribution of logits using nucleus (top-p) filtering.
-
-        Source: https://gist.github.com/thomwolf/1a5a29f6962089e871b94cbd09daf317
-        Nucleus filtering is described in Holtzman et al. (http://arxiv.org/abs/1904.09751)
-
-        Args:
-            logits: logits distribution of shape (batch size, vocabulary size).
-            top_p >0.0: keep the top tokens with cumulative probability >= top_p (nucleus filtering).
-
-        Returns:
-            logits of top tokens.
-        """
-        if 0.0 < top_p < 1.0:
-            sorted_logits, sorted_indices = torch.sort(logits, descending=True)
-            cumulative_probs = torch.cumsum(F.softmax(sorted_logits, dim=-1), dim=-1)
-
-            # Remove tokens with cumulative probability above the threshold (token with 0 are kept)
-            sorted_indices_to_remove = cumulative_probs > top_p
-
-            # Shift the indices to the right to keep also the first token above the threshold
-            sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[
-                                                ..., :-1
-                                                ].clone()
-            sorted_indices_to_remove[..., 0] = 0
-
-            # scatter sorted tensors to original indexing
-            indices_to_remove = sorted_indices_to_remove.scatter(
-                1, sorted_indices, sorted_indices_to_remove
-            )
-            logits[indices_to_remove] = filter_value
-
-        return logits
-
     def _rescale_positions(self, events: list[Event]) -> list[Event]:
         new_events = []
         offset = self.position_precision // 2 if self.position_precision > 1 else 0
