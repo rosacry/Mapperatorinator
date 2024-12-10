fix float precision

osuT5/osuT5/dataset/osu_parser.py
@@ -354,7 +354,7 @@ class OsuParser:
             events.append(Event(EventType.NEW_COMBO))
             event_times.append(time_ms)
         if scroll_speed is not None:
-            events.append(Event(EventType.SCROLL_SPEED, int(np.clip(scroll_speed, 0.01, 10) * 100)))
+            events.append(Event(EventType.SCROLL_SPEED, round(np.clip(scroll_speed, 0.01, 10) * 100)))
             event_times.append(time_ms)
         if hitsound_ref_times is not None:
             for i, ref_time in enumerate(hitsound_ref_times):

osuT5/osuT5/tokenizer/tokenizer.py
@@ -436,7 +436,7 @@ class Tokenizer:
 
     def encode_global_sv(self, global_sv: float) -> int:
         """Converts global sv into token id."""
-        return self.encode(Event(type=EventType.GLOBAL_SV, value=int(np.clip(global_sv, 0.4, 3.6) * 100)))
+        return self.encode(Event(type=EventType.GLOBAL_SV, value=round(np.clip(global_sv, 0.4, 3.6) * 100)))
 
     def _init_beatmap_idx(self, args: DictConfig) -> None:
         """Initializes and caches the beatmap index."""