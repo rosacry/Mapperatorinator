b7fe29e7fa1a65e5d394bf9bc2267934a33d5aa2
OliBomby <olivierschipper@gmail.com>
2025-05-17 13:59:05 +0200

    remove training attention mask to enable flash attention sdpa backend

--- osuT5/osuT5/dataset/mmrs_dataset.py
+++ osuT5/osuT5/dataset/mmrs_dataset.py
@@ -641,7 +641,7 @@ class BeatmapDatasetIterable:
         #                               input_tokens)
 
         sequence["decoder_input_ids"] = input_tokens
-        sequence["decoder_attention_mask"] = input_tokens != self.tokenizer.pad_id
+        # sequence["decoder_attention_mask"] = input_tokens != self.tokenizer.pad_id
         sequence["labels"] = label_tokens
 
         del sequence["out_context"]
